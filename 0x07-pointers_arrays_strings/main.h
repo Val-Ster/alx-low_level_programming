eb0f072c7b9ea9b0eb85f6ae2ccd416d4be5330e
Valerie Igbokwe <Vster310@gmail.com>
2023-07-03 16:17:18 -0700

    0x07. C - Even more pointers, arrays and strings.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-int_putchar(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsighed int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsighed int_strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
