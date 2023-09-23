11448dc70c2ac34f395ab2f852e2d4eccc8f9bc4
Valerie Igbokwe <Vster310@gmail.com>
2023-09-22 18:23:03 -0700

    C functions that can be called from Python

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -15,23 +15,13 @@ int mul(int a, int b)
 	return (a * b);
 }
 
-int divide(int a, int b)
+int div(int a, int b)
 {
-	if (b == 0)
-	{
-		/*you can choose to Handle division by zero error.*/
-		return (-1); /*Here, we'll return -1 to indicate the error.*/
-	}
 	return (a / b);
 }
 
 int mod(int a, int b)
 {
-	if (b == 0)
-	{
-		/*you can choose to Handle modulus by zero error.*/
-		return (-1); /*Here, we'll return -1 to indicate the error.*/
-	}
 	return (a % b);
 }
 
