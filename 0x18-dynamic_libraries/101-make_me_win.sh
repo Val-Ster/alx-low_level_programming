345644dcefd2412508d75fc934a89397a9d09b18
Valerie Igbokwe <Vster310@gmail.com>
2023-09-24 07:46:40 -0700

    dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -P /tmp https://github.com/Val-Ster/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjected_code.so
-export LD_PRELOAD=/tmp/libinjected_code.so
+wget -P /tmp https://github.com/Emmanueldev247/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
