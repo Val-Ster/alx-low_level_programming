a5ef333b9676bd2461a99eb322e40c4fded173bc
Val-Ster <vster310@gmail.com>
2023-11-13 17:31:17 -0800

    changing 100-test.py from python to C language

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: the first integer operand
+ * @b: the second integer operand
+ *
+ * Return: The result of the addition operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: the first integer operand
+ * @b: the second integer operand
+ *
+ * Return: The result of the subtraction operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer operand
+ * @b: the second integer operand
+ *
+ * Return: The result of the multiplication operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * division - divides two integers
+ * @a: the first integer operand
+ * @b: the second integer operand
+ *
+ * Return: The result of the division operation
+ */
+int division(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the remainder of  two integers after division
+ * @a: the first integer operand
+ * @b: the second integer operand
+ *
+ * Return: The result of the modulus operation
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
